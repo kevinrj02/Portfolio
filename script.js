9285417b40ed15ba8b3408f1c5de7382f5e80628
kevinrj02 <137980414+kevinrj02@users.noreply.github.com>
2025-01-10 17:41:01 -0500

    Removed the Horizontal Scroll version

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-  
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+const preventHorizontalScroll = () => {
+  document.body.style.overflowX = "hidden";
+  document.documentElement.style.overflowX = "hidden";
+
+  document.querySelectorAll('*').forEach(element => {
+      const rect = element.getBoundingClientRect();
+      if (rect.right > window.innerWidth) {
+          element.style.maxWidth = "100vw";
+          element.style.boxSizing = "border-box";
+      }
+  });
+};
+
+preventHorizontalScroll();
